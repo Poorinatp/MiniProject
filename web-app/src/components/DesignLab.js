51a0a51606deeb327cb9925895678d83ad0dbe6d
Poorinat Padungyan <s6404062630511@gmail.com>
2023-10-04 23:39:45 +0700

    design lab 6

--- web-app/src/components/DesignLab.js
+++ web-app/src/components/DesignLab.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
-const DesignLab = ({ textData, setTextData, isSelected, setIsSelected }) => {
+const DesignLab = ({ 
+  textData, 
+  setTextData,
+  imageData, 
+  setImageData, 
+  isSelected, 
+  setIsSelected,
+  selectedImage
+ }) => {
   const canvasRef = useRef(null);
   const isClicked = useRef(false);
+  const rotationAngleRef = useRef(0);
   const [currentElement, setcurrentElement] = useState(null);
   
   const imgDesign = [
@@ -38,7 +49,7 @@ const DesignLab = ({ textData, setTextData, isSelected, setIsSelected }) => {
 
     const onMouseMove = (e) => {
       if (!isClicked.current) return;
-    
+
       const nextX = e.clientX - coords.current.startX + coords.current.lastX;
       const nextY = e.clientY - coords.current.startY + coords.current.lastY;
     
@@ -49,13 +60,11 @@ const DesignLab = ({ textData, setTextData, isSelected, setIsSelected }) => {
     currentElement.addEventListener("mousedown", onMouseDown);
     currentElement.addEventListener("mouseup", onMouseUp);
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mouseleave", onMouseUp);
 
     const cleanup = () => {
       currentElement.removeEventListener("mousedown", onMouseDown);
       currentElement.removeEventListener("mouseup", onMouseUp);
       canvas.removeEventListener("mousemove", onMouseMove);
-      canvas.removeEventListener("mouseleave", onMouseUp);
     };
     return cleanup;
   }, [currentElement]);
@@ -65,7 +74,18 @@ const DesignLab = ({ textData, setTextData, isSelected, setIsSelected }) => {
   }, [textData])
   const handleTextChange = (index, e) => {
     const updatedTextData = [...textData];
+    const valueLength = e.target.value.length;
+    console.log("length = "+valueLength);
+    console.log("fontSize = "+valueLength*parseInt(updatedTextData[index].fontSize,10))
+    console.log("fontSize = "+valueLength*(parseInt(updatedTextData[index].fontSize,10))/1.618)
+    const newWidth = `${valueLength*(parseInt(updatedTextData[index].fontSize,10))/1.618}px`;
     updatedTextData[index].value = e.target.value;
+    console.log(newWidth);
+    console.log("Before:")
+    console.log(updatedTextData);
+    updatedTextData[index].width = newWidth;
+    console.log("After:")
+    console.log(updatedTextData);
     setTextData(updatedTextData);
   };
 
@@ -73,61 +93,146 @@ const DesignLab = ({ textData, setTextData, isSelected, setIsSelected }) => {
     const updatedSelection = Array(textData.length).fill(false)
     updatedSelection[index] = !updatedSelection[index];
     setIsSelected(updatedSelection);
-    console.log(isSelected)
+    //console.log(isSelected)
   };
   
+  const handleRotate = (index, e) => {
+    const updatedTextData = [...textData];
+    const newRotationAngles = textData[index].rotationAngle + 45;
+    updatedTextData[index].rotationAngle += 45;
+    setTextData(updatedTextData);
+    e.target.style.transform = `rotate(${newRotationAngles[index]}deg)`; // Apply rotation
+  };
 
   return (
-    <div className="grid-item">
-      <img src='../image/tshirt.jpg' alt="t-shirt" className="tshirt" />
-      <div ref={canvasRef} className="canvas" style={{width:"210px", height:"340px"}} 
-      onClick={e=>{
-        currentElement?currentElement.style.border = "transparent":console.log(e.target);
-      }}>
-      {textData.map((text, index) => (
-        <div
-          key={"textbox" + text.id}
-          id={"textbox" + text.id}
-          className="textbox"
-          onClick={e=>{
-            currentElement!==e.target&&currentElement!==null?currentElement.style.border = "transparent":console.log(e.target);
-            handleItemClick(index, text.id);
-            setcurrentElement(e.target)
-            e.target.style.border = "2px dashed black";
-          }}
-          onMouseEnter={e=>{
-            e.target.style.border = "2px dashed black";
-            e.target.style.padding = "4px";
-          }}
-          onMouseLeave={e=>{
-            if (currentElement!==e.target){
-              e.target.style.border = "transparent";
-              e.target.style.padding = "6px";
-            }
-          }}
-          >
-          <input 
-            key={"textinput" + text.id}
-            id={"textinput" + text.id}
-            className="textinput"
-            style={{ 
-              top: text.x,
-              left: text.y,
-              fontFamily: text.fontFamily,
-              fontSize: text.fontSize,
-              color: text.fontColor
-             }}
-            
-            onChange={e => {
-              const newWidth = e.target.value.length;
-              e.target.style.width = `${newWidth*text.fontSize}px`;
-              handleTextChange(index, e)
+    <div className="grid-item-1 lab">
+      <div className="container-1"
+      >
+        <img src='../image/tshirt.jpg' alt="t-shirt" className="tshirt" />
+        <div className="canvas"
+        ref={canvasRef}
+        onClick={e=>{
+          currentElement?currentElement.style.border = "transparent":console.log(e.target);
+        }}>
+        {textData.map((text, index) => (
+          <div>
+            <div
+              key={`textbox${text.id}`}
+              id={`textbox${text.id}`}
+              className="textbox"
+              >
+              <input 
+                key={"textinput" + text.id}
+                id={"textinput" + text.id}
+                className="textinput"
+                style={{
+                  width: text.width,
+                  top: text.x,
+                  left: text.y,
+                  padding:"6px",
+                  transform: `rotate(${text.rotationAngle}deg)`,
+                  fontFamily: text.fontFamily,
+                  fontSize: text.fontSize,
+                  color: text.fontColor,
+                }}
+                
+                onChange={e => {
+                  handleTextChange(index, e)
+                }}
+                value={text.value}
+                onClick={e=>{
+                  handleItemClick(index, text.id);
+                  setcurrentElement(e.target)
+                  e.target.style.border = "2px dashed black";
+                }}
+                onMouseEnter={e=>{
+                  e.target.style.border = "2px dashed black";
+                  e.target.style.padding = "4px";
+                }}
+                onMouseLeave={e=>{
+                  if (currentElement!==e.target){
+                    e.target.style.border = "transparent";
+                    e.target.style.padding = "6px";
+                  }
+                }}
+              />
+              
+            </div>
+            {/* {isSelected[index]?
+            <button
+            className="rotatebtn"
+            onClick={e=>{
+              const deltaX = e.clientX - coords.current.startX;
+              const deltaY = e.clientY - coords.current.startY;
+              const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+              rotationAngleRef.current += angle;
+              currentElement.style.transform = `rotate(${rotationAngleRef.current}deg)`;
+              coords.current.startX = e.clientX;
+              coords.current.startY = e.clientY;
             }}
-            value={text.value}
-          />
+            onMouseMove={e=>{
+              const deltaX = e.clientX - coords.current.startX;
+              const deltaY = e.clientY - coords.current.startY;
+              const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+              rotationAngleRef.current += angle;
+              currentElement.style.transform = `rotate(${rotationAngleRef.current}deg)`;
+              coords.current.startX = e.clientX;
+              coords.current.startY = e.clientY;
+            }}
+            style={{
+              top:currentElement.style.top,
+              left:currentElement.style.left
+            }}
+          >
+            <FontAwesomeIcon icon={faRotateLeft} style={{ color: "#0042aa" }} />
+          </button>:<></>
+            } */}
+          </div>
+          ))}
+          {imageData.map((image, index) => (
+          <div>
+            <div
+              key={`imgbox${image.id}`}
+              id={`imgbox${image.id}`}
+              className="imgbox"
+              style={{
+                width: image.width,
+                top: image.x,
+                left: image.y,
+                padding:"6px",
+                transform: `rotate(${image.rotationAngle}deg)`,
+              }}
+              draggable="false"
+              onClick={e=>{
+                handleItemClick(index, image.id);
+                setcurrentElement(e.target)
+                e.target.style.border = "2px dashed black";
+                e.target.style.padding = "4px";
+              }}
+              onMouseEnter={e=>{
+                e.target.style.border = "2px dashed black";
+                e.target.style.padding = "4px";
+              }}
+              onMouseLeave={e=>{
+                if (currentElement!==e.target){
+                  e.target.style.border = "transparent";
+                  e.target.style.padding = "6px";
+                }
+              }}
+              >
+              <img
+                src="blob:http://localhost:3000/b3158cc4-d925-4325-89d9-76162dde110e"
+                alt="Display Image"
+                className="display-image"
+                style={{width:"100%"}}
+                draggable="false"
+              />
+            </div>
+          </div>
+          ))}
         </div>
-        ))}
       </div>
+      
     </div>
   );
 };
--- web-app/src/pages/Design.js
+++ web-app/src/pages/Design.js
@@ -3,7 +3,18 @@ import DesignLab from "../components/DesignLab"
 import NavBar from "../components/NavBar";
 import "./Design.css"
 import DesignLab2 from "../components/DesignLab2";
-const OptionTab = ({textData, setTextData, isSelected, setIsSelected, handleFontChange, handleSizeChange, handleColorChange}) => {
+const OptionTab = ({
+    textData, 
+    setTextData,
+    imageData, 
+    setImageData, 
+    isSelected, 
+    setIsSelected, 
+    selectedImage, 
+    handleFontChange, 
+    handleSizeChange, 
+    handleColorChange,
+    handleImageUpload}) => {
     const [selectedOption, setSelectedOption] = useState("product");
     const size = Array.from({ length: 50 }, (_, i) => (i + 12) * 2);
 
@@ -11,7 +22,7 @@ const OptionTab = ({textData, setTextData, isSelected, setIsSelected, handleFont
         setSelectedOption(option);
     };
 
-    const handleAddObject = () => {
+    const handleAddText = () => {
         const newObject = {
             id:textData.length, 
             value: "new text", 
@@ -23,13 +34,25 @@ const OptionTab = ({textData, setTextData, isSelected, setIsSelected, handleFont
         };
 
         setTextData((prevObjects) => [...prevObjects, newObject]);
-      };
+    };
+    const handleAddImage = (image) => {
+        const newObject = {
+            id: 1, 
+            width:"50px", 
+            x: "0px", 
+            y: "0px", 
+            rotationAngle: 0, 
+            image: selectedImage,
+            filename: selectedImage.name
+        }
+        setImageData((prevObjects) => [...prevObjects, newObject]);
+    };
       
 
     return(
         <div className="grid-item">
-            <div className="grid-container">
-                <div className="grid-item option-tab">
+            <div className="grid-container-2">
+                <div className="grid-item-2 option-tab">
                     <button
                     className={selectedOption === "product" ? "active" : ""}
                     onClick={() => handleOptionChange("product")}
@@ -50,9 +73,9 @@ const OptionTab = ({textData, setTextData, isSelected, setIsSelected, handleFont
                     </button>
                 </div>
                 {/* Render content based on selectedOption */}
-                {selectedOption === "product" && <div className="grid-item option-content">Product Option Content</div>}
+                {selectedOption === "product" && <div className="grid-item-3 option-content">Product Option Content</div>}
                 {selectedOption === "text" && 
-                <div className="grid-item option-content">        
+                <div className="grid-item-3 option-content">        
                     <div>
                         {textData.map((text)=>{
                             return(
@@ -61,7 +84,7 @@ const OptionTab = ({textData, setTextData, isSelected, setIsSelected, handleFont
                                 </p>
                             )
                         })}
-                        <button onClick={handleAddObject}>Create New Object</button>
+                        <button onClick={handleAddText}>Create New Object</button>
                     </div>
                     {isSelected.some((selected) => selected === true)&&
                         <div>
@@ -87,66 +110,110 @@ const OptionTab = ({textData, setTextData, isSelected, setIsSelected, handleFont
                             </select>
 
                             <p>Font Color</p>
-                            <input
-                            type="text"
-                            value={textData[isSelected.findIndex((selected) => selected === true)].fontColor}
-                            onChange={(e) => handleColorChange(e, isSelected.findIndex((selected) => selected === true))}
-                            placeholder="Enter color code..."
-                            />
+                            <div className="colorpicker">
+                                <input
+                                type="color"
+                                value={textData[isSelected.findIndex((selected) => selected === true)].fontColor}
+                                onChange={(e) => handleColorChange(e, isSelected.findIndex((selected) => selected === true))}
+                                />
+                                <input
+                                type="text"
+                                className="colortextinput"
+                                value={textData[isSelected.findIndex((selected) => selected === true)].fontColor}
+                                onChange={(e) => handleColorChange(e, isSelected.findIndex((selected) => selected === true))}
+                                placeholder="Enter color code..."
+                                />
+                            </div>
                         </div>
                     }
                 </div>}
-                {selectedOption === "image" && <div className="grid-item option-content">Image Option Content</div>}
+                {selectedOption === "image" && 
+                    <div className="grid-item-4 option-content">
+                        <input
+                        type="file"
+                        accept="image/*"
+                        onChange={e=>handleImageUpload(e)}
+                        />
+                        {selectedImage && (
+                            <>
+                            <img
+                                src={URL.createObjectURL(selectedImage)}
+                                alt="Uploaded Image"
+                                className="uploaded-image"
+                            />
+                            <button onClick={handleAddImage}>Add image</button>
+                            </>
+                        )}
+                    </div>}
             </div>
         </div>
     )
 }
 
 const Design = () => {
-    const [textData,setTextData] = useState([
-        {id:1, value: "hi", fontFamily:"Arial", fontSize:"10px", fontColor:"black", x:"0px", y:"0px"},
-        {id:2, value: "mynameis", fontFamily:"Arial", fontSize:"10px", fontColor:"red", x:"50px", y:"50px"},
-        {id:3, value: "poom", fontFamily:"Arial", fontSize:"10px", fontColor:"blue", x:"100px", y:"100px"}
-    ])
+    const [textData, setTextData] = useState([
+        {id: 1, value: "hi", fontFamily: "Arial", width:"50px", fontSize: "24px", fontColor: "black", x: "0px", y: "0px", rotationAngle: 0},
+        {id: 2, value: "mynameis", fontFamily: "Arial", width:"100px", fontSize: "24px", fontColor: "red", x: "50px", y: "50px", rotationAngle: 0},
+        {id: 3, value: "poom", fontFamily: "Arial", width:"100px", fontSize: "24px", fontColor: "blue", x: "100px", y: "100px", rotationAngle: 0}
+    ]);      
     const [isSelected, setIsSelected] = useState(Array(textData.length).fill(false));
+    
+    const [imageData, setImageData] = useState([
+        {id: 1, width:"50px", x: "0px", y: "0px", rotationAngle: 0, image: "blob:http://localhost:3000/b3158cc4-d925-4325-89d9-76162dde110e", filename:""}
+    ]);
+
+    const [selectedImage, setSelectedImage] = useState(null);
 
     const handleFontChange = (event, index) => {
         const newTextElements = [...textData];
         newTextElements[index].fontFamily = event.target.value;
         setTextData(newTextElements);
-      };
+    };
     
-      const handleSizeChange = (event, index) => {
+    const handleSizeChange = (event, index) => {
         const newTextElements = [...textData];
         newTextElements[index].fontSize = event.target.value;
         setTextData(newTextElements);
-      };
+    };
     
-      const handleColorChange = (event, index) => {
+    const handleColorChange = (event, index) => {
         const newTextElements = [...textData];
         newTextElements[index].fontColor = event.target.value;
         setTextData(newTextElements);
-      };
+    };
+
+    const handleImageUpload = (e) => {
+        const imageFile = e.target.files[0];
+        setSelectedImage(imageFile);
+        console.log(selectedImage);
+    };
 
     return(
         <>
             <NavBar/>
-            <div className="grid-container">
+            <div className="grid-container-1">
                 <OptionTab
                     textData={textData}
-                    isSelected={isSelected}
                     setTextData={setTextData}
+                    imageData={imageData}
+                    setImageData={setImageData}
+                    isSelected={isSelected}
                     setIsSelected={setIsSelected}
+                    selectedImage={selectedImage}
                     handleFontChange={handleFontChange}
                     handleSizeChange={handleSizeChange}
                     handleColorChange={handleColorChange}
+                    handleImageUpload={handleImageUpload}
                 />
                 {/* <DesignLab/> */}
                 <DesignLab
                     textData={textData}
                     isSelected={isSelected}
+                    imageData={imageData}
+                    setImageData={setImageData}
                     setTextData={setTextData}
                     setIsSelected={setIsSelected}
+                    selectedImage={selectedImage}
                 />
             </div>
         </>
