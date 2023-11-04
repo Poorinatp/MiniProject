a714ca34ac51aa4f1db4ec7c6723ba9fc0d04353
tthanaphon <s6404062630384@email.kmutnb.ac.th>
2023-11-05 00:10:59 +0700

    showDesign-Homepage

--- web-app/src/pages/Home.js
+++ web-app/src/pages/Home.js
@@ -1,16 +1,33 @@
 import "./Home.css"
 import {useNavigate} from 'react-router-dom';
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import NavBar from "../components/NavBar";
+import Axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" ></meta>
  
-const Home = () => {
+const Home = ({apihost}) => {
 	const navigate = useNavigate();
-
+	const [exDesignList, setexDesignList] = useState([]);
+	
 	const navigateToDesign =()=>{
 		navigate('/design')
 	}
-
+	const handleEditProduct = (product_id) => {
+		navigate(`/design/${product_id}`)
+	  };
+	useEffect(()=>{
+		Axios.get(`${apihost}/product/admin`)
+			.then((result) => {
+				setexDesignList(result.data);
+				console.log(result)
+			})
+			.catch((error) => {
+				console.error('Error fetching user data:', error);
+			});
+	}
+	)
 	return (
 		<main >
 			<NavBar/>
@@ -24,6 +41,23 @@ const Home = () => {
 
 				<button className="btn-home" onClick={navigateToDesign}>start Do it!</button>
 			</div>
+			<article className="card-example" >
+              {exDesignList.map((exDesignList, key) => {
+                return (
+                  <section className="card-body-example" key={key}>
+                    <img src={exDesignList.product_image} alt='img-his'/>
+					<article className='btn-group'>
+                      <FontAwesomeIcon
+                        className='icon-btn'
+                        size='2xs'
+                        icon={faPen}
+                        onClick={() => handleEditProduct(exDesignList.Product_id)}
+                      />
+                    </article>
+                  </section>
+                );
+              })}
+            </article>
 		</main>
 	  );
 	};
