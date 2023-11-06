c002f974dcc6fb584d0141be640aa82e1cd7e6b1
Poorinat Padungyan <s6404062630511@gmail.com>
2023-11-07 03:22:53 +0700

    test api upload file to server 3

--- web-api/server2.js
+++ web-api/server2.js
@@ -50,17 +50,9 @@ const pool = new Pool({
 
 const shirtDesignStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log("req")
-    console.log(req? req:"no req")
-    console.log("file")
-    console.log(file? file:"no file")
     cb(null, 'https://pimniyom.onrender.com/web-app/public/shirt-design');
   },
   filename: (req, file, cb) => {
-    console.log("req")
-    console.log(req? req:"no req")
-    console.log("file")
-    console.log(file? file:"no file")
     cb(null, file.originalname);
   },
 });
